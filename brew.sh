44860a2350fbf5917d07a146299d4dd8c774473f
Ken Herner <chosenken@gmail.com>
2014-12-05 22:54:50 -0500

    Added verbos flag cause I like seeing the configs happen :P

--- brew.sh
+++ brew.sh
@@ -40,7 +40,6 @@ brew install -v wget --with-iri
 brew install -v ringojs
 brew install -v narwhal
 
-<<<<<<< 0e18dcf272a647edb2898b9f58591d6a0bdb004e
 # Install more recent versions of some macOS tools.
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
@@ -101,58 +100,5 @@ brew install tree
 brew install vbindiff
 brew install webkit2png
 brew install zopfli
-
-=======
-# Install more recent versions of some OS X tools.
-brew install -v vim --override-system-vi
-brew install -v homebrew/dupes/grep
-brew install -v homebrew/dupes/screen
-brew install -v homebrew/php/php55 --with-gmp
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install -v bfg
-brew install -v binutils
-brew install -v binwalk
-brew install -v cifer
-brew install -v dex2jar
-brew install -v dns2tcp
-brew install -v fcrackzip
-brew install -v foremost
-brew install -v hashpump
-brew install -v hydra
-brew install -v john
-brew install -v knock
-brew install -v nmap
-brew install -v pngcheck
-brew install -v socat
-brew install -v sqlmap
-brew install -v tcpflow
-brew install -v tcpreplay
-brew install -v tcptrace
-brew install -v ucspi-tcp # `tcpserver` etc.
-brew install -v xpdf
-brew install -v xz
-
-# Install other useful binaries.
-brew install -v ack
-#brew install -v exiv2
-brew install -v git
-brew install -v imagemagick --with-webp
-brew install -v lua
-brew install -v lynx
-brew install -v p7zip
-brew install -v pigz
-brew install -v pv
-brew install -v rename
-brew install -v rhino
-brew install -v tree
-brew install -v webkit2png
-brew install -v zopfli
-
-# Install Node.js. Note: this installs `npm` too, using the recommended
-# installation method.
-brew install -v node
-
->>>>>>> Added verbos flag cause I like seeing the configs happen :P
 # Remove outdated versions from the cellar.
 brew cleanup
